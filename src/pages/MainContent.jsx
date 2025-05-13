b6274842ab81d0fbad5ae43be6be4b395923bdfb
Aziz Dafa Putra <dafaputra@student.uns.ac.id>
2025-05-13 09:39:16 +0700

    change bg image

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -4,20 +4,26 @@ import Location from '@/pages/Location';
 import Wishes from '@/pages/Wishes';
 import Gifts from '@/pages/Gifts';
 import Stories from './Stories';
+import berduaan from '../assets/lamaran-rumput.jpg';
 
 // Main Invitation Content
 export default function MainContent() {
     return (
-        <>
-            <Hero />
-            <Events />
-            <Location />
-            <Stories />
-            <Gifts />
-            <Wishes />
-            <footer className="text-center text-sm text-zinc-500 py-4 mb-8">
-            Template by <a href="https://github.com/mrofisr" className="hover:underline" target="_blank" rel="noopener noreferrer">Abdur Rofi</a>, Modified by <a href="https://github.com/dafaputra00" className="hover:underline" target="_blank" rel="noopener noreferrer">Aziz</a> 💙 <a href="https://github.com/khonsaazmi" className="hover:underline" target="_blank" rel="noopener noreferrer">Khonsa</a>
-            </footer>
-        </>
+        <div
+            className='min-h-screen w-full bg-contain bg-center'
+            style={{ backgroundImage: `url(${berduaan})` }}
+        >
+            <div className="bg-white/70 backdrop-blur-0 min-h-screen w-full py-10">
+                <Hero />
+                <Events />
+                <Location />
+                <Stories />
+                <Gifts />
+                <Wishes />
+                <footer className="text-center text-sm text-zinc-500 py-4 mb-8">
+                Template by <a href="https://github.com/mrofisr" className="hover:underline" target="_blank" rel="noopener noreferrer">Abdur Rofi</a>, Modified by <a href="https://github.com/dafaputra00" className="hover:underline" target="_blank" rel="noopener noreferrer">Aziz</a> 💙 <a href="https://github.com/khonsaazmi" className="hover:underline" target="_blank" rel="noopener noreferrer">Khonsa</a>
+                </footer>
+            </div>
+        </div>
     )
 }
