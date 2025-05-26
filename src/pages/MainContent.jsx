932187f8bdf530bf2b2d345f0679c7f138b11009
Khonsa <khonsaazmi@gmail.com>
2025-05-26 17:29:45 +0700

    fix whitespace in main content

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -1,29 +1,55 @@
-import Hero from '@/pages/Hero'
-import Events from '@/pages/Events'
-import Location from '@/pages/Location';
-import Wishes from '@/pages/Wishes';
-import Gifts from '@/pages/Gifts';
-import Stories from './Stories';
-import berduaan from '../assets/lamaran-rumput.jpg';
+import Hero from "@/pages/Hero";
+import Events from "@/pages/Events";
+import Location from "@/pages/Location";
+import Wishes from "@/pages/Wishes";
+import Gifts from "@/pages/Gifts";
+import Stories from "./Stories";
+import berduaan from "../assets/lamaran-rumput.jpg";
 
 // Main Invitation Content
 export default function MainContent() {
-    return (
-        <div
-            className='min-h-screen w-full bg-contain bg-center'
-            style={{ backgroundImage: `url(${berduaan})` }}
-        >
-            <div className="bg-white/70 backdrop-blur-0 min-h-screen w-full py-10">
-                <Hero />
-                <Events />
-                <Location />
-                <Stories />
-                <Gifts />
-                <Wishes />
-                <footer className="text-center text-sm text-zinc-500 py-4 mb-8">
-                Template by <a href="https://github.com/mrofisr" className="hover:underline" target="_blank" rel="noopener noreferrer">Abdur Rofi</a>, Modified by <a href="https://github.com/dafaputra00" className="hover:underline" target="_blank" rel="noopener noreferrer">Aziz</a> 💙 <a href="https://github.com/khonsaazmi" className="hover:underline" target="_blank" rel="noopener noreferrer">Khonsa</a>
-                </footer>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className="min-h-screen w-full bg-contain bg-center"
+      // style={{ backgroundImage: `url(${berduaan})` }}
+    >
+      <div className="bg-white/70 backdrop-blur-0 min-h-screen w-full">
+        <Hero />
+        <Events />
+        <Location />
+        <Stories />
+        <Gifts />
+        <Wishes />
+        <footer className="text-center text-sm text-zinc-500 py-4 mb-8">
+          Template by{" "}
+          <a
+            href="https://github.com/mrofisr"
+            className="hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Abdur Rofi
+          </a>
+          , Modified by{" "}
+          <a
+            href="https://github.com/dafaputra00"
+            className="hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Aziz
+          </a>{" "}
+          💙{" "}
+          <a
+            href="https://github.com/khonsaazmi"
+            className="hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Khonsa
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
