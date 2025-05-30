a045253905783f05fcb56c076351e7c4311ef67c
Khonsa <khonsaazmi@gmail.com>
2025-05-30 14:14:33 +0700

    switch location

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -15,12 +15,12 @@ export default function Location() {
     <>
       {/* Location section */}
       <section id="location" className="min-h-screen relative overflow-hidden">
-        <div className="container mx-auto px-4 py-20 relative z-10">
+        <div className="container mx-auto px-4 py-40 relative z-10">
           {/* Section Header */}
           <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 }}
+            initial={{ opacity: 0, scale: 0.9 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
             viewport={{ once: true }}
             className="text-center space-y-4 mb-16"
           >
@@ -50,7 +50,7 @@ export default function Location() {
               whileInView={{ scale: 1 }}
               transition={{ delay: 0.4 }}
               viewport={{ once: true }}
-              className="flex items-center justify-center gap-4 pt-4"
+              className="flex items-center justify-center gap-4 pt-5"
             >
               <div className="h-[1px] w-12 bg-slate-200" />
               <MapPin className="w-5 h-5 text-slate-400" />
@@ -60,26 +60,6 @@ export default function Location() {
 
           {/* Location Content */}
           <div className="max-w-6xl mx-auto grid md:grid-row-2 gap-8 items-center">
-            {/* Map Container */}
-            <motion.div
-              initial={{ opacity: 0, x: -50 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.8 }}
-              viewport={{ once: true }}
-              className="w-full h-[400px] rounded-2xl overflow-hidden shadow-lg border-8 border-white"
-            >
-              <iframe
-                src={config.data.maps_embed}
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                className="w-full h-full"
-              ></iframe>
-            </motion.div>
-
             {/* Venue Details */}
             <motion.div
               initial={{ opacity: 0, x: 50 }}
@@ -101,17 +81,17 @@ export default function Location() {
                     </p>
                   </div>
 
-                  <div className="flex items-center space-x-4">
+                  {/* <div className="flex items-center space-x-4">
                     <CalendarCheck className="w-5 h-5 text-slate-500" />
                     <p className="text-gray-600">
                       {formatEventDate(config.data.date)}
                     </p>
-                  </div>
+                  </div> */}
 
-                  <div className="flex items-center space-x-4">
+                  {/* <div className="flex items-center space-x-4">
                     <Clock className="w-5 h-5 text-slate-500" />
                     <p className="text-gray-600">{config.data.time}</p>
-                  </div>
+                  </div> */}
 
                   {/* Action Button - Full Width */}
                   <div className="pt-4">
@@ -130,6 +110,26 @@ export default function Location() {
                   </div>
                 </div>
               </div>
+            </motion.div>
+
+            {/* Map Container */}
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+              viewport={{ once: true }}
+              className="w-full h-[400px] rounded-2xl overflow-hidden shadow-lg border-8 border-white"
+            >
+              <iframe
+                src={config.data.maps_embed}
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className="w-full h-full"
+              ></iframe>
             </motion.div>
           </div>
         </div>
