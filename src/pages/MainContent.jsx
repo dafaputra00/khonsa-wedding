6606b3a3e5941f4186b29503fd1be8e9d258e642
Khonsa <khonsaazmi@gmail.com>
2025-05-30 14:14:47 +0700

    edit footer

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -4,8 +4,10 @@ import Location from "@/pages/Location";
 import Wishes from "@/pages/Wishes";
 import Gifts from "@/pages/Gifts";
 import Stories from "./Stories";
+import Gallery from "@/pages/Gallery";
 import background1 from "../assets/background2.png";
 import { motion } from "framer-motion";
+import lamaran4 from "../assets/lamaran4.png"; // Uncomment if needed
 
 // Main Invitation Content
 export default function MainContent() {
@@ -24,37 +26,49 @@ export default function MainContent() {
         <Hero />
         <Events />
         <Location />
+        <Gallery />
         <Stories />
         <Gifts />
         <Wishes />
-        <footer className="text-center text-sm text-zinc-500 py-4 mb-8">
-          Template by{" "}
-          <a
-            href="https://github.com/mrofisr"
-            className="hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Abdur Rofi
-          </a>
-          , Modified by{" "}
-          <a
-            href="https://github.com/dafaputra00"
-            className="hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Aziz
-          </a>{" "}
-          💙{" "}
-          <a
-            href="https://github.com/khonsaazmi"
-            className="hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Khonsa
-          </a>
+        <footer
+          className="text-center text-sm text-white mb-0"
+          style={{
+            backgroundImage: `linear-gradient(to bottom,rgb(226, 232, 240),rgba(255, 255, 255, 0.16), #ffffff00), url(${lamaran4})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            height: "55vh",
+          }}
+        >
+          <div className="absolute bottom-0 left-0 right-0 p-4 ">
+            Template by{" "}
+            <a
+              href="https://github.com/mrofisr"
+              className="hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Abdur Rofi
+            </a>
+            , Modified by{" "}
+            <a
+              href="https://github.com/dafaputra00"
+              className="hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Khonsa
+            </a>{" "}
+            💙{" "}
+            <a
+              href="https://github.com/khonsaazmi"
+              className="hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Aziz
+            </a>
+          </div>
         </footer>
       </motion.div>
     </div>
