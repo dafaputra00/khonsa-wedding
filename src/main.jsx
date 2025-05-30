647a31ae866724736966b77fac9e34e5ff277fdd
Khonsa <khonsaazmi@gmail.com>
2025-05-30 16:29:52 +0700

    tambah gift registry

--- src/main.jsx
+++ src/main.jsx
@@ -17,12 +17,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Gift from "./GiftRegistry.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <Gift />
   </StrictMode>
 );
--- src/pages/Gifts.jsx
+++ src/pages/Gifts.jsx
@@ -79,7 +79,7 @@ export default function Gifts() {
               <div className="space-y-2 my-6">
                 <p className="text-gray-800 text-lg">List Referensi Hadiah</p>
                 <a
-                  href="https://shorturl.at/6QzKr"
+                  href="/src/GiftRegistry.jsx"
                   target="_blank"
                   className="hover:underline text-gray-600 italic text-base"
                   rel="noopener noreferrer"
